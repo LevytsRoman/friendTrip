f7f6dc6916f11d1f48e9b7b37596888eabc4b2b6
sforsell <mskforsell@gmail.com>
2017-09-17 10:10:04 -0400

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/', to: 'api#home', as: 'home'
-  root 'api#test'
+  root '/', to: 'api#home', as: 'home'
 end
